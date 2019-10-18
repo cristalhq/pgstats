28e73eccad03f1208115d999bddfc751499394d3
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-18 19:57:03 +0200

    add version parsing

--- pgstats.go
+++ pgstats.go
@@ -2,7 +2,12 @@ package pgstats
 
 import (
 	"database/sql"
+	"errors"
+	"regexp"
+	"strconv"
 )
+
+var versionRegex = regexp.MustCompile(`(^9\.\d)|(^\d{2})`)
 
 // Stats provides an access to the Postgres monitoring statistics.
 type Stats struct {
@@ -23,4 +28,25 @@ func New(db *sql.DB) (*Stats, error) {
 // Close closes the connection to the sdatabase.
 func (s *Stats) Close() error {
 	return s.db.Close()
+}
+
+func (s *Stats) getVersion() (float64, error) {
+	const query = "SHOW server_version;"
+	row := s.db.QueryRow(query)
+
+	var version string
+	err := row.Scan(&version)
+	if err != nil {
+		return 0, err
+	}
+	return parseMajorVersion(version)
+}
+
+func parseMajorVersion(s string) (float64, error) {
+	v := versionRegex.FindString(s)
+	if v == "" {
+		return 0, errors.New("Regex parse error")
+	}
+
+	return strconv.ParseFloat(v, 64)
 }
