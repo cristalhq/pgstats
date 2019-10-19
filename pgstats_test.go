5a6f4b6bca57f9bf44af098207ac6c7d4c1c0f6a
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-10-19 13:09:36 +0200

    add testing suite (#1)

---
+++ pgstats_test.go
@@ -0,0 +1,65 @@
+package pgstats
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var host, port, dbname, user, pass, mode string
+
+var testConn *sql.DB
+
+func init() {
+	flag.StringVar(&host, "host", "127.0.0.1", "test host")
+	flag.StringVar(&port, "port", "5432", "test port")
+	flag.StringVar(&dbname, "dbname", "postgres_db", "test db name")
+	flag.StringVar(&user, "user", "postgres_user", "test username")
+	flag.StringVar(&pass, "pass", "postgres_pass", "test pass")
+	flag.StringVar(&mode, "mode", "disable", "test mode")
+
+	connString := fmt.Sprintf(`
+	host=%s port=%s
+	user=%s password=%s
+	dbname=%s
+	sslmode=%s`, host, port, user, pass, dbname, mode)
+
+	fmt.Printf("test connection: %v\n\n", connString)
+
+	var err error
+	testConn, err = sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
+	if err := testConn.Ping(); err != nil {
+		panic(err)
+	}
+}
+
+func noErr(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func isOK(t *testing.T, size int, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if size == 0 {
+		t.Fatal("No data from query")
+	}
+}
+
+func TestArchiver(t *testing.T) {
+	stats, err := New(testConn)
+	noErr(t, err)
+
+	_, err = stats.Archiver()
+	isOK(t, 1, err)
+}
